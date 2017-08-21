6b3cd27e83d65eccaef90374e4f252c246e93141
Javier Vazquez <jvrvzqzp@gmail.com>
2017-08-21 13:23:41 -0600

    Returning absolute url, crawler almost working

--- crawler.py
+++ crawler.py
@@ -1,14 +1,17 @@
 #Initialize libraries
 import sqlite3
 import ssl
-import lesscode_lib as lcode
+import sys
+from urllib.request import urlopen
+from urllib.parse import urlparse, urljoin
+import functions as lcode
+from bs4 import BeautifulSoup
 #End Libraries
 
 #ssl is anoying, ignore it
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode=ssl.CERT_NONE
-
 
 
 conn = sqlite3.connect('Crawled.DB')
@@ -32,15 +35,15 @@ url TEXT UNIQUE
 
 CREATE TABLE IF NOT EXISTS Links(
 from_id INTEGER,
-to_id INTEGER
+to_id INTEGER UNIQUE
 )
 ''')
 
 #Cleaning input url
 web_site = input('Type webpage to crawl\n')
-site_page = lcode.clean_url(web_site, ctx) #Returns html/text
+site_page, web_site = lcode.clean_url(web_site, ctx) #Returns HTTPResponse object
 
-#find "likness"
+
 pos = web_site.rfind('.')
 clean = web_site[:pos]
 pos = clean.rfind('www.')
@@ -68,5 +71,90 @@ webs = []
 for row in cur:
 	webs.append(str(row[0]))
 print(webs)
+
+
+many = 0
+while True:
+	if (many < 1):
+		scal = input('How many pages: ')
+		if (len(scal) < 1): break
+		many = int(scal)
+	many -= 1
+#Verifying if url was gathered already
+	cur.execute('''SELECT id,url FROM Pages WHERE html IS NULL AND error is NULL
+	ORDER BY RANDOM() LIMIT 1 ''')
+	try:
+		row = cur.fetchone()
+		fromid = row[0]
+		url = row[1]
+	except:
+		print('No unretrieve HTML pages found')
+		many = 0
+		break
+	print(fromid, url, end=' ')
+#Checking for errors on site, or during process (network, interruption)
+	try:
+		cur.execute('DELETE FROM Links WHERE from_id = ?',(fromid,))
+		html = site_page.read()
+		if site_page.getcode() != 200:
+			print('Error on page', site_page.getcode())
+			cur.execute('UPDATE Pages SET error=? WHERE url=?',
+					(site_page.getcode(), url))
+		if 'text/html' != site_page.info().get_content_type():
+			print('Ignore non text/html page')
+			cur.execute('DELETE FROM Pages WHERE url=?',(url,))
+			continue
+		soup = BeautifulSoup(html, 'html.parser')
+		print('Characters ('+str(len(html))+')', end =' ')
+	except KeyboardInterrupt:
+		print('Interrupted')
+		break
+	except:
+		print('Unable to retrieve or parse')
+		cur.execute('UPDATE Pages SET error=-1 WHERE url=?',(url, ))
+		conn.commit()
+		continue
+
+#Done checking for errors, lets get the info
+
+	cur.execute('''
+	INSERT OR IGNORE INTO Pages(url, html, new_rank) VALUES(?, NULL, 1.0) ''',
+	(url,))
+	cur.execute('UPDATE Pages SET html=? WHERE url=?',(memoryview(html),url))
+	conn.commit()
+#Retrieving new urls
+	tags = soup('a')
+	count = 0
+	for tag in tags:
+		href = tag.get('href', None)
+		if (href is None): continue
+	#Solve weird references like href'/facultad/2017'
+		up = urlparse(href)
+		if (len(up.scheme)<1):
+			href=urljoin(url,href)
+		ipos = href.find('#')
+		if(ipos>1):href = href[:pos]
+		if (href.endswith('/')): href = href[:-1]
+		print (href)
+
+
+
+
+		cur.execute('''
+		INSERT OR IGNORE INTO Pages (url, html, new_rank) VALUES (?,NULL,1.0)''',
+		(href, ))
+		count += 1
+		conn.commit()
+
+		cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', (href,))
+		try:
+			row = cur.fetchone()
+			toid=row[0]
+		except:
+			print('Could not retrieve ID')
+			continue
+		cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES (?,?)',
+					(fromid, toid))
+	print(count)
 
 cur.close()
---
+++ functions.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen, Request
+from urllib.error import URLError
+import sys
+
+def clean_url(web_site, ctx): # returns HTTP object
+	headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'}
+
+	if (web_site.endswith('/') ) :
+		web_site = web_site[:-1]
+	if ( web_site.endswith('.htm') or web_site.endswith('.html') ) :
+		pos = web_site.rfind('/')
+		web_site = web_site[:pos]
+	if not (web_site.startswith('http://') or web_site.startswith('https://')):
+		try:
+			req = Request('https://'+web_site, headers=headers)
+			doc = urlopen(req, context=ctx)
+			url = 'https://'+web_site
+		except:
+				try:
+					req = Request('http://'+web_site, headers=headers)
+					doc = urlopen(req, context=ctx)
+					url = 'http://'+web_site
+				except URLError as error:
+					print('Unable to retrive ', error)
+					sys.exit(0)
+	else:
+		try:
+			req = Request(web_site, headers=headers)
+			doc = urlopen(req, context=ctx)
+			url = web_site
+		except URLError as error:
+			print ('Unable to retrieve ', error)
+			sys.exit(0)
+	return doc, url
