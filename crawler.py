c75ad465129c239cd3ff49af1b0e217efdb2adad
javiervqz <jvrvzqzp@gmail.com>
2018-01-26 12:18:51 -0700

    Fixed commit to DB problem

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 #Initialize libraries
 import sqlite3
 from classes import LinkParser
+import sys
 #End Libraries
 
 
@@ -26,12 +27,13 @@ url TEXT UNIQUE
 
 CREATE TABLE IF NOT EXISTS Links(
 from_id INTEGER,
-to_id INTEGER UNIQUE
+to_id INTEGER
 )
 ''')
 
 #Cleaning input url outputs html and url
 web_site = input('Type website to crawl\n')
+many = int(input('How many?'))
 BaseWeb = LinkParser()
 html, BaseUrl = BaseWeb.CleanUrl(web_site)
 
@@ -48,41 +50,47 @@ else:#You can add the continuation of the crawling here
 	print('Restarting existing crawl \n')
 
 i = 0
-while i < 30:
+while i < many:
 	links = []
-	print('Getting links from page {}'.format(i))
 	cur.execute('''SELECT html, url FROM Pages WHERE error = 120 ORDER BY RANDOM() LIMIT 1''')
 	row = cur.fetchone()
+	if row is None:
+		print('No more pages to visit')
+		break
 	html = row[0]
 	BaseUrl = row[1]
+	print('Getting links from page {}'.format(BaseUrl))
 	print ('{}, {}'.format(len(html), BaseUrl))
 	cur.execute('''UPDATE Pages SET error = 100 WHERE url = ? ''', (BaseUrl,))
-	links = BaseWeb.GetLinks(html)
+	links = BaseWeb.GetLinks(html,web_site)
 	cur.execute('''SELECT id FROM Pages WHERE url = ? ''', (BaseUrl,))
 	from_id = cur.fetchone()[0]
-	print (links)
+	print ('Links found',len(links))
+	j = 0
 	for link in links:
-		html, BaseUrl = BaseWeb.CleanUrl(link)
+		try:
+			html, BaseUrl = BaseWeb.CleanUrl(link)
+		except:
+			continue
 		if BaseUrl == "None html page":
 			print ("Ignored page (not html)")
 			continue
 		elif html is None:
 			cur.execute('''INSERT OR IGNORE INTO Pages (url, error) VALUES (?, 150)''', (BaseUrl,))
-			conn.commit()
 		else:
-			print ("Getting new page")
 			cur.execute('''INSERT OR IGNORE INTO Pages (url, html, new_rank, id_website, error) VALUES (?, ?, 1.0, ?, 120) ''', (BaseUrl, html, id_website))
-			conn.commit()
 
 		cur.execute('''SELECT id FROM Pages WHERE url = ? ''', (BaseUrl,))
 		to_id = cur.fetchone()[0]
 		if from_id != to_id:
-			cur.execute('''INSERT OR IGNORE INTO Links (from_id, to_id) VALUES (?, ?)''', (from_id, to_id))
-			conn.commit()
+			cur.execute('''INSERT INTO Links (from_id, to_id) VALUES (?, ?)''', (from_id, to_id))
+			continue
+		j += 1
+	conn.commit()
 
 	i += 1
 
 
 
-conn.commit()
+# conn.commit()
 cur.close()
