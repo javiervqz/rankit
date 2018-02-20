fb7a1efe1980c1fa26f214e0b5f45e7c07511e0d
javiervqz <jvrvzqzp@gmail.com>
2018-02-20 10:54:39 -0700

    Set to default values, normalized to 100

---
+++ reset_rank.py
@@ -0,0 +1,20 @@
+import sqlite3
+from numpy import around
+conn = sqlite3.connect('Crawled.db')
+cur = conn.cursor()
+
+cur.execute('SELECT id FROM Pages')
+no_pages = len(cur.fetchall())
+start = 100.0/no_pages
+
+cur.execute('UPDATE Pages SET new_rank = ? ', (start,))
+
+cur.execute('SELECT SUM(new_rank) FROM Pages')
+result = around([cur.fetchone()[0]], decimals=1)
+if result[0] != 100.0:
+	print ('''There is data lost or reset did not run correctly\n
+			Pagerank total {}'''.format(result[0]))
+conn.commit()
+cur.close()
+
+print ("{} pages set to rank {}".format(no_pages, start))
