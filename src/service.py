2ade22600bb8b5bbab81700c264eb9f374ee1070
Javier <jvrvzqzp@gmail.com>
2022-02-11 19:07:27 -0600

    change variable convention and return in public method

--- src/service.py
+++ src/service.py
@@ -1,4 +1,30 @@
-
+from sqlite3 import connect
 
 class crawlService:
-    pass
+    def __init__(self,table_name):
+        self.table_name=table_name
+
+    def _initializeDB(self):
+        sqlConn = connect(self.table_name) 
+        cur = sqlConn.cursor()
+        cur.executescript('''
+        CREATE TABLE IF NOT EXISTS Pages(
+        id INTEGER PRIMARY KEY,
+        url TEXT UNIQUE,
+        html TEXT,
+        error INTEGER,
+        old_rank REAL,
+        new_rank REAL,
+        id_website INTEGER
+        );
+
+        CREATE TABLE IF NOT EXISTS Links(
+        from_id INTEGER,
+        to_id INTEGER
+        )
+        '''
+        )
+
+        sqlConn.commit()
+        cur.close()
+
